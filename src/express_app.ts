80709f5bb0c927c2552f85786a571ddd7edef6d1
Chris Dietsch <cdietschrunfast@gmail.com>
2023-10-08 21:52:36 +1300

    other path

--- src/express_app.ts
+++ src/express_app.ts
@@ -109,7 +109,7 @@ export async function StartExpressServer()
   
   if (process.env.NODE_ENV?.trim() === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
