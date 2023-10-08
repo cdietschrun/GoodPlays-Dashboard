d8dee458cc17893a1b2a854dad4e51e118843504
Chris Dietsch <cdietschrunfast@gmail.com>
2023-10-08 23:36:30 +1300

    remove/ignore

--- dist/routes/connections/mongo.js
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-const connectionString = process.env.ATLAS_URI || `mongodb+srv://cdietschrunfast:${process.env.MONGO_DB_PASSWORD}@goodplays.yhu6h4r.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(connectionString);
-await client.connect();
-const db = client.db("goodplays_database");
-const playsCollection = db.collection("game_play");
-export default playsCollection;
